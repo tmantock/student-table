7416ffad7cd92f7b422239785cb147f4513382ee
Tevin Mantock <tmantock@gmail.com>
2016-12-21 22:47:41 -0800

    working react components with react bootstrap

--- client/src/app/components/header/header.js
+++ client/src/app/components/header/header.js
@@ -1,33 +1,36 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 require('./header.scss');
 
 class Header extends Component {
-    render(){
+    render() {
         return (
-            <nav className = "navbar navbar-light" id="navbar">
-                <div className = "container-fluid">
-                    <div className="navbar-header">
-                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#react-redux-nav-collapse" aria-expanded="false">
-                            <span className="sr-only">Toggle navigation</span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                        </button>
-                        <Link to="/" className="navbar-brand">React Redux</Link>
-                    </div>
-                    <div className="collapse navbar-collapse" id="react-redux-nav-collapse">
-                        <ul className = "nav navbar-nav navbar-right">
-                            <li className = "nav-item" key={1}>
-                                <Link className="nav-link" to="/signin">Sign-In</Link>
-                            </li>
-                            <li className="nav-item" key={2}>
-                                <Link className="nav-link" to="/signup">Sign-Up</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+            <Navbar inverse collapseOnSelect>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="#">React-Bootstrap</a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <NavItem eventKey={1} href="#">Link</NavItem>
+                        <NavItem eventKey={2} href="#">Link</NavItem>
+                        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                            <MenuItem eventKey={3.1}>Action</MenuItem>
+                            <MenuItem eventKey={3.2}>Another action</MenuItem>
+                            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                            <MenuItem divider />
+                            <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav pullRight>
+                        <NavItem eventKey={1} href="#">Link Right</NavItem>
+                        <NavItem eventKey={2} href="#">Link Right</NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         );
     }
 }
