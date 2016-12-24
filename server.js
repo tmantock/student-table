60a840cf1777e0680100fceb879f7afbfe0d27c3
Tevin Mantock <tmantock@gmail.com>
2016-12-23 22:18:41 -0800

    beautiify js

--- server.js
+++ server.js
@@ -13,15 +13,15 @@ const compiler = webpack(config);
 app.use(express.static(__dirname + '/dist'));
 app.use(webpackMiddleware(compiler));
 app.use(webpackHotMiddleware(compiler));
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     var fileStream = fs.createReadStream(path.join(__dirname, 'client/src/public/index.html'));
     res.statusCode = 200;
     fileStream.pipe(res);
-    
+
     var dataLength = 0;
 
     fileStream
-        .on('data', function (chunk){
+        .on('data', function (chunk) {
             dataLength += chunk.length;
         })
         .on('end', function () {
@@ -29,7 +29,7 @@ app.get('/', function(req, res){
         });
 });
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log(`===========================`);
     console.log(`Listening on PORT: ${PORT}`);
     console.log(`===========================`);
