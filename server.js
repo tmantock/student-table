9ea6c0de83e20768af808596a0946528dbe6c13e
Tevin Mantock <tmantock@gmail.com>
2016-12-23 22:01:26 -0800

    sending a file over a stream

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 const express = require('express');
 const webpack = require('webpack');
 const webpackMiddleware = require('webpack-dev-middleware');
@@ -13,7 +14,19 @@ app.use(express.static(__dirname + '/dist'));
 app.use(webpackMiddleware(compiler));
 app.use(webpackHotMiddleware(compiler));
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, 'client/src/public/index.html'));
+    var fileStream = fs.createReadStream(path.join(__dirname, 'client/src/public/index.html'));
+    res.statusCode = 200;
+    fileStream.pipe(res);
+    
+    var dataLength = 0;
+
+    fileStream
+        .on('data', function (chunk){
+            dataLength += chunk.length;
+        })
+        .on('end', function () {
+            console.log(`The length was: ${dataLength}`);
+        });
 });
 
 app.listen(PORT, function(){
